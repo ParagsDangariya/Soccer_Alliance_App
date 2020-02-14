1c937cbd2146a919be9d77d4f60a98c13bd5afd3
Saurabh Jain <saurabhjain3699@gmail.com>
2020-02-14 12:54:00 -0500

    Schedule match XML added.

---
+++ app/src/main/java/com/example/soccerallianceapp/ScheduleMatchFragment.java
@@ -0,0 +1,27 @@
+package com.example.soccerallianceapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.soccer_alliance_project_test.R;
+
+
+public class ScheduleMatchFragment extends Fragment {
+    public ScheduleMatchFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_schedule_match, container, false);
+    }
+}
