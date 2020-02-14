87807bea771cece263316675c0a97f3e851e0046
ParekhAmi <amiparekh87>
2020-02-13 23:31:46 -0500

    create new league XML added

---
+++ app/src/main/java/com/example/soccerallianceapp/CreateLeagueFragment.java
@@ -0,0 +1,30 @@
+package com.example.soccerallianceapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.soccer_alliance_project_test.R;
+
+
+public class CreateLeagueFragment extends Fragment {
+
+
+    public CreateLeagueFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_create_league, container, false);
+    }
+
+}
