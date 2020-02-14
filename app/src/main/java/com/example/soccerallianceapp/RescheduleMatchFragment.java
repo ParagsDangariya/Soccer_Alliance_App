b3c5b3227ca8facb7c2169648aa940623da45fa2
Saurabh Jain <saurabhjain3699@gmail.com>
2020-02-14 13:15:22 -0500

    Reschedule match XML added.

---
+++ app/src/main/java/com/example/soccerallianceapp/RescheduleMatchFragment.java
@@ -0,0 +1,27 @@
+package com.example.soccerallianceapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.soccer_alliance_project_test.R;
+
+
+
+public class RescheduleMatchFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reschedule_match, container, false);
+    }
+
+}
