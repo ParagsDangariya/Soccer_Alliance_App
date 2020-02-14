a1ff2a5456333e5a1b0c04a3d8e08531d4b77b84
Saurabh Jain <saurabhjain3699@gmail.com>
2020-02-14 13:25:21 -0500

    Match Score Reschedule XML added.

---
+++ app/src/main/java/com/example/soccerallianceapp/MatchScoreRescheduleFragment.java
@@ -0,0 +1,25 @@
+package com.example.soccerallianceapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.soccer_alliance_project_test.R;
+
+
+public class MatchScoreRescheduleFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_match_score_reschedule, container, false);
+    }
+
+}
