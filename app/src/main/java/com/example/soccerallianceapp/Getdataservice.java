baaf679b66b55c78a86b2f7da5a9582781ea2557
princyvahora <vohrafarhin102@gmail.com>
2020-02-21 13:38:50 -0500

    Retrofit library added

---
+++ app/src/main/java/com/example/soccerallianceapp/Getdataservice.java
@@ -0,0 +1,12 @@
+package com.example.soccerallianceapp;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface Getdataservice {
+
+//    @GET("{geoId}")
+//    Call<Weather> getWeather(@Path("geoId")int geoId);
+
+}
