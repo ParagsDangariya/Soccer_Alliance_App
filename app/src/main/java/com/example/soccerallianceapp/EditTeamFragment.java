b238ff8caa634b6cbf8e2e1738051f0e1bee0c9d
princyvahora <vohrafarhin102@gmail.com>
2020-03-04 13:38:09 -0500

    edit player fragment XML Layout.

--- app/src/main/java/com/example/soccerallianceapp/EditTeamFragment.java
+++ app/src/main/java/com/example/soccerallianceapp/EditTeamFragment.java
@@ -31,12 +31,10 @@ import retrofit2.Response;
 public class EditTeamFragment extends Fragment implements View.OnClickListener{
 
     public NavController DashboardNavController;
-    private RecyclerView player_recycler_view;
+
     private Context context;
     Getdataservice service;
-    private ArrayList<Comman_Data_List> comman_data_List;
-    private Comman_adapter comman_adapter;
-    int team_id = 17;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,57 +54,14 @@ public class EditTeamFragment extends Fragment implements View.OnClickListener{
         DashboardNavController = Navigation.findNavController(getActivity(),R.id.dashboard_host_fragment);
         context = getActivity().getApplicationContext();
 
-        comman_data_List = new ArrayList<Comman_Data_List>();
+
 
         service = RetroFitInstance.getRetrofitInstance().create(Getdataservice.class);
 
 
-        /*--------Teams Adapter Configuration--------*/
-        player_recycler_view = view.findViewById(R.id.player_recycler_view);
-        comman_data_List = new ArrayList<Comman_Data_List>();
-        comman_data_List.clear();
-
-        comman_adapter = new Comman_adapter(comman_data_List, context);
-        getPlayerlist(team_id,service);
-        player_recycler_view.setLayoutManager(new LinearLayoutManager(context));
-        player_recycler_view.setAdapter(comman_adapter);
 
 
-    }
-    private void getPlayerlist(int team_id, Getdataservice service) {
 
-        Call viewPlayerLIstByTeam = service.getviewPlayerListFromTeamDashboardCall(team_id);
-        System.out.println("Before respon");
-        viewPlayerLIstByTeam.enqueue(new Callback<ViewPlayerListDashboard>() {
-            @Override
-            public void onResponse(Call<ViewPlayerListDashboard> call, Response<ViewPlayerListDashboard> response) {
-
-                ViewPlayerListDashboard playerListDashboard = response.body();
-                System.out.println("after get response "+response.body());
-                if(response.body() != null){
-                    if (playerListDashboard.getStatus() == 200) {
-                        System.out.println("status from player frag "+playerListDashboard.getStatus());
-                        for (PlayerList playerList : playerListDashboard.getPlayerList()) {
-
-                            comman_data_List.add(new Comman_Data_List(
-                                    playerList.getFullName(),
-                                    playerList.getPlayerPhoto(),
-                                    playerList.getStrength()
-                            ));
-                        }
-                        comman_adapter.notifyDataSetChanged();
-                    }
-
-                }else{
-                    Toast.makeText(getActivity() ,"Response empty",Toast.LENGTH_LONG).show();
-                }
-            }
-            @Override
-            public void onFailure(Call call, Throwable t) {
-                System.out.println("Error : "+t.getMessage());
-
-            }
-        });
     }
 
     @Override
