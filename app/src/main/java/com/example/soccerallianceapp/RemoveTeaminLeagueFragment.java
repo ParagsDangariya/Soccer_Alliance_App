4fb1b416fa4e5a257c1c41e3c8eafc801335d03c
Parag Dangriya <45040382+ParagsDangariya@users.noreply.github.com>
2020-03-04 11:28:43 -0500

    fragments are added.

---
+++ app/src/main/java/com/example/soccerallianceapp/RemoveTeaminLeagueFragment.java
@@ -0,0 +1,17 @@
+package com.example.soccerallianceapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class RemoveTeaminLeagueFragment extends Fragment {
+
+}
