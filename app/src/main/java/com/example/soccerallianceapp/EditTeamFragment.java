709dca662cf8efdf538784eae7032d9fcf50f183
princyvahora <vohrafarhin102@gmail.com>
2020-03-04 13:29:18 -0500

    edit player fragment.

--- app/src/main/java/com/example/soccerallianceapp/EditTeamFragment.java
+++ app/src/main/java/com/example/soccerallianceapp/EditTeamFragment.java
@@ -4,109 +4,113 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.soccer_alliance_project_test.R;
+import com.example.soccerallianceapp.pojo.ViewPlayerListByTeamDashboard.PlayerList;
+import com.example.soccerallianceapp.pojo.ViewPlayerListByTeamDashboard.ViewPlayerListDashboard;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link EditTeamFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link EditTeamFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class EditTeamFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class EditTeamFragment extends Fragment implements View.OnClickListener{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public EditTeamFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditTeamFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EditTeamFragment newInstance(String param1, String param2) {
-        EditTeamFragment fragment = new EditTeamFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    public NavController DashboardNavController;
+    private RecyclerView player_recycler_view;
+    private Context context;
+    Getdataservice service;
+    private ArrayList<Comman_Data_List> comman_data_List;
+    private Comman_adapter comman_adapter;
+    int team_id = 17;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_edit_team, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        DashboardNavController = Navigation.findNavController(getActivity(),R.id.dashboard_host_fragment);
+        context = getActivity().getApplicationContext();
+
+        comman_data_List = new ArrayList<Comman_Data_List>();
+
+        service = RetroFitInstance.getRetrofitInstance().create(Getdataservice.class);
+
+
+        /*--------Teams Adapter Configuration--------*/
+        player_recycler_view = view.findViewById(R.id.player_recycler_view);
+        comman_data_List = new ArrayList<Comman_Data_List>();
+        comman_data_List.clear();
+
+        comman_adapter = new Comman_adapter(comman_data_List, context);
+        getPlayerlist(team_id,service);
+        player_recycler_view.setLayoutManager(new LinearLayoutManager(context));
+        player_recycler_view.setAdapter(comman_adapter);
+
+
+    }
+    private void getPlayerlist(int team_id, Getdataservice service) {
+
+        Call viewPlayerLIstByTeam = service.getviewPlayerListFromTeamDashboardCall(team_id);
+        System.out.println("Before respon");
+        viewPlayerLIstByTeam.enqueue(new Callback<ViewPlayerListDashboard>() {
+            @Override
+            public void onResponse(Call<ViewPlayerListDashboard> call, Response<ViewPlayerListDashboard> response) {
+
+                ViewPlayerListDashboard playerListDashboard = response.body();
+                System.out.println("after get response "+response.body());
+                if(response.body() != null){
+                    if (playerListDashboard.getStatus() == 200) {
+                        System.out.println("status from player frag "+playerListDashboard.getStatus());
+                        for (PlayerList playerList : playerListDashboard.getPlayerList()) {
+
+                            comman_data_List.add(new Comman_Data_List(
+                                    playerList.getFullName(),
+                                    playerList.getPlayerPhoto(),
+                                    playerList.getStrength()
+                            ));
+                        }
+                        comman_adapter.notifyDataSetChanged();
+                    }
+
+                }else{
+                    Toast.makeText(getActivity() ,"Response empty",Toast.LENGTH_LONG).show();
+                }
+            }
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                System.out.println("Error : "+t.getMessage());
+
+            }
+        });
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+    public void onClick(View v) {
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
