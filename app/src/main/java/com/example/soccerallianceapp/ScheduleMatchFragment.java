bc0bce3246a492adbe403fac237ec8a443e01d04
Saurabh Jain <saurabhjain3699@gmail.com>
2020-03-06 13:04:55 -0500

    Add validation on input fields in schedule match Screen.

--- app/src/main/java/com/example/soccerallianceapp/ScheduleMatchFragment.java
+++ app/src/main/java/com/example/soccerallianceapp/ScheduleMatchFragment.java
@@ -3,6 +3,7 @@ package com.example.soccerallianceapp;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -50,8 +51,9 @@ public class ScheduleMatchFragment extends Fragment implements View.OnClickListe
     FirebaseAuth fAuth;
     Getdataservice service;
     MaterialButton Schedule_match_btn;
+    ArrayList<TeamList> testlist;
 
-    Integer League_id = 1;
+    int League_id;
     String uid = "";
     int day, month, year;
 
@@ -64,7 +66,6 @@ public class ScheduleMatchFragment extends Fragment implements View.OnClickListe
     String location;
 
     int team1id, team2id;
-    int league_id = 1;
     String stime;
     String team1name = "", team2name = "";
 
@@ -90,6 +91,14 @@ public class ScheduleMatchFragment extends Fragment implements View.OnClickListe
         fAuth = FirebaseAuth.getInstance();
         comman_data_List = new ArrayList<Comman_Data_List>();
         comman_adapter = new Comman_adapter(comman_data_List, context);
+        try {
+            if (getArguments() != null) {
+                League_id = getArguments().getInt("League_id");
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         schedule_match_edt_txt = (AutoCompleteTextView) view.findViewById(R.id.schedule_match_edt_txt);
         schedule_match_team2_edt_txt = (AutoCompleteTextView) view.findViewById(R.id.schedule_match_team2_edt_txt);
@@ -102,120 +111,65 @@ public class ScheduleMatchFragment extends Fragment implements View.OnClickListe
 
         // get Current Date
         Calendar calendar = Calendar.getInstance();
-
         day = calendar.get(Calendar.DAY_OF_MONTH);
         month = calendar.get(Calendar.MONTH);
         year = calendar.get(Calendar.YEAR);
 
-
-                schedule_match_date_edt_txt.setOnClickListener(new View.OnClickListener() {
+        schedule_match_date_edt_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog dialog = DatePickerDialog.newInstance(new DatePickerDialog.OnDateSetListener() {
                     @Override
-                    public void onClick(View v) {
-//
-
-                        DatePickerDialog dialog = DatePickerDialog.newInstance(new DatePickerDialog.OnDateSetListener() {
-                            @Override
-                            public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
-
-                                String strdate = year + "-" + (monthOfYear+1) + "-" + dayOfMonth;
-                                  schedule_match_date_edt_txt.setText(strdate);
-
-                            }
-                        },year,month,day);
-
-
-                        dialog.show(getActivity().getFragmentManager(),"DatePicketDialog");
-
-
-
-
-
+                    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+                        String strdate = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
+                        schedule_match_date_edt_txt.setText(strdate);
                     }
-                });
-
-
-
-
+                }, year, month, day);
+                dialog.show(getActivity().getFragmentManager(), "DatePicketDialog");
+            }
+        });
         schedule_match_time_layout_edt_txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 DialogFragment timepicker = new TimePickerFragment();
-
                 timepicker.show(getActivity().getSupportFragmentManager(), "time Picker");
-
-
             }
         });
-
-
         uid = fAuth.getCurrentUser().getUid();
-        Toast.makeText(getActivity(), "UID : " + uid, Toast.LENGTH_LONG).show();
+        //Toast.makeText(getActivity(), "UID : " + uid, Toast.LENGTH_LONG).show();
         System.out.println("User Id : " + uid);
 
-
         service = RetroFitInstance.getRetrofitInstance().create(Getdataservice.class);
-
-
-        // Call<ScheduleMatch> scheduleMatchCall = service.scheduleMatch(scdulematch);
-
-
-        Log.d("step1", "after getService part");
-
-        //League_id = getArguments().getInt("League_id");
-
-//        Toast.makeText(context, "Schedule MAtch Fragment (League Id ): " + League_id, Toast.LENGTH_LONG).show();
-
+        Log.d("step1", "after getService part (View match list by leagueID)");
 
         Call<ViewTeamListFromLeagueId> call = service.viewTeamListFromLeagueId(League_id);
-
-        System.out.println(call);
-
         call.enqueue(new Callback<ViewTeamListFromLeagueId>() {
             @Override
             public void onResponse(Call<ViewTeamListFromLeagueId> call, Response<ViewTeamListFromLeagueId> response) {
-
                 Log.d("Response : ", " " + response);
-
                 ViewTeamListFromLeagueId maindata = response.body();
-
-                System.out.println("Maindata" + maindata);
-
-
+                testlist = new ArrayList<>(maindata.getTeamList());
+                System.out.println("TestList :" + testlist);
                 if (response.body() != null) {
-
                     //   List<TeamList> teamLists = response.body().getTeamList();
-
                     for (TeamList teamList : maindata.getTeamList()) {
                         comman_data_List.add(new Comman_Data_List(
-
-
                                 teamList.getTeamid(),
                                 teamList.getTeamName(),
                                 teamList.getLogo()
-
                         ));
-
                     }
-
-
                     String[] listofteam = new String[comman_data_List.size()];
-
 
                     for (int i = 0; i < comman_data_List.size(); i++) {
                         listofteam[i] = comman_data_List.get(i).getItem_name();
-
-
                         System.out.print("teamlists." + listofteam[i]);
                     }
-
 
                     ArrayAdapter<String> teamadapter = new ArrayAdapter<>(context,
                             simple_selectable_list_item, listofteam);
 
-
                     schedule_match_edt_txt.setAdapter(teamadapter);
-
 
                     ArrayAdapter<String> teamadapter2 = new ArrayAdapter<>(context,
                             simple_selectable_list_item, listofteam);
@@ -225,136 +179,128 @@ public class ScheduleMatchFragment extends Fragment implements View.OnClickListe
                     schedule_match_edt_txt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                         @Override
                         public void onItemClick(AdapterView<?> parent, View arg1, int position, long id) {
-
                             Object item = parent.getItemIdAtPosition(position);
-
                             System.out.println("Team1 id (Selected from Dropdown) : " + item);
-
-
+                            Log.i("Test Size",String.valueOf(testlist.size()));
+                            checkMe(testlist);
                         }
                     });
 
                     schedule_match_team2_edt_txt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                         @Override
                         public void onItemClick(AdapterView<?> parent, View view, int p, long id) {
-
                             team2id = comman_data_List.get(p).getIteam_id();
-
                             System.out.println("Team2 id SM 2 : " + team2id);
-
-
+                            checkMe(testlist);
                         }
                     });
                 } else {
                     Toast.makeText(getActivity(), "Response empty", Toast.LENGTH_LONG).show();
-
                 }
-
             }
-
             @Override
             public void onFailure(Call<ViewTeamListFromLeagueId> call, Throwable t) {
-
+                System.out.println("On Failure in ViewTeamList" + t);
             }
         });
-
-
     }
-
-
     @Override
     public void onClick(View view) {
-
         if (view == Schedule_match_btn) {
 
+            team1name = schedule_match_edt_txt.getText().toString();
+            team2name = schedule_match_team2_edt_txt.getText().toString();
+            date = schedule_match_date_edt_txt.getText().toString();
+            location = schedule_match_location_layout_edt_txt.getText().toString();
+            time = schedule_match_time_layout_edt_txt.getText().toString();
 
-            date = schedule_match_date_edt_txt.getEditableText().toString();
-
-            location = schedule_match_location_layout_edt_txt.getEditableText().toString();
-
-            time = schedule_match_time_layout_edt_txt.getEditableText().toString();
-
-
+            if(TextUtils.isEmpty(team1name)){
+                schedule_match_edt_txt.setError("Team 1 is Required.");
+                return ;
+            }else if(TextUtils.isEmpty(team2name)){
+                schedule_match_team2_edt_txt.setError("Team 2 is Required");
+                return;
+            }else if(TextUtils.isEmpty(date)) {
+                schedule_match_date_edt_txt.setError("Date is Required.");
+                return;
+            }else if(TextUtils.isEmpty(time)) {
+                schedule_match_time_layout_edt_txt.setError("Time is Required.");
+                return;
+            }else if(TextUtils.isEmpty(location)) {
+                schedule_match_location_layout_edt_txt.setError("Location is Required.");
+                return;
+            }
+            else if(team1name.equals(team2name)){
+                schedule_match_team2_edt_txt.setError("Team 1 and Team 2 must be Different!");
+                return;
+            }
         }
 
-
-        //league_id = getArguments().getInt("League_id");
-
-        Getdataservice service1 = RetroFitInstance.getRetrofitInstance().create(Getdataservice.class);
-
-
-        String url = "https://soccerallianceapp.appspot.com/rest/api/CreateSchedule&" + location + "&" + date + "&" + time + "&" + team1id + "&" + team2id + "&" + league_id + "";
-
-
+        String url = "https://soccerallianceapp.appspot.com/rest/api/CreateSchedule&" + location + "&" + date + "&" + time + "&" + team1id + "&" + team2id + "&" + League_id + "";
         System.out.println("url" + url);
 
-        ScheduleMatch schedulematch = new ScheduleMatch(location, date, time, team1id, team2id, league_id);
-
-
-        System.out.println("Team1 id (Selected from Dropdown) : " + team1id);
-
-        System.out.println("Team2 id (Selected from Dropdown) : " + team2id);
+        ScheduleMatch schedulematch = new ScheduleMatch(location, date, time, team1id, team2id, League_id);
 
         System.out.println("Schedulematch : " + schedulematch.toString());
         try {
-
-            Call<ScheduleMatch> data = service1.SCHEDULE_MATCH_CALL(schedulematch);
-
+            Call<ScheduleMatch> data = service.SCHEDULE_MATCH_CALL(schedulematch);
             System.out.println("data : " + data);
-
             data.enqueue(new Callback<ScheduleMatch>() {
                 @Override
                 public void onResponse(Call<ScheduleMatch> data, Response<ScheduleMatch> response) {
-
                     if (!response.isSuccessful()) {
                         int sm = response.code();
                         System.out.println("code" + sm);
                         Toast.makeText(context, "Schedule Match Successfully " + sm, Toast.LENGTH_LONG).show();
-
-
                     }
 
                     int sm = response.code();
                     System.out.println("code" + sm);
-
-
                     Toast.makeText(context, "Schedule Match Successfully " + sm, Toast.LENGTH_LONG).show();
-
-
                     //DashboardNavController.navigate(R.id.leagueOperationsFragment);
-
                 }
-
-
                 @Override
                 public void onFailure(Call<ScheduleMatch> data, Throwable t) {
-
-
                     System.out.println("error" + t.getMessage());
                     Toast.makeText(context, "Wrong thing happened", Toast.LENGTH_LONG).show();
-
                 }
             });
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
-
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-
-
         stime = hourOfDay + ":" + minute;
-
         schedule_match_time_layout_edt_txt.setText("H : " + hourOfDay + "M: " + minute);
-
         System.out.println("Ontime set : " + schedule_match_date_edt_txt);
+    }
+    public void checkMe(ArrayList<TeamList> testlist1)
+    {
+        team1name = schedule_match_edt_txt.getText().toString();
+        team2name = schedule_match_team2_edt_txt.getText().toString();
+        for (int x=0;x<testlist1.size();x++)
+        {
+            Log.i("Test Size in for loop",String.valueOf(testlist1.size()));
+            System.out.println("Team Name" + schedule_match_edt_txt.getText());
+
+            if (testlist1.get(x).getTeamName().equals(team1name))
+            {
+                team1id =  testlist.get(x).getTeamid();
+                System.out.println("Found team1 id : " + team1id);
+                //Log.i("Id of team",testlist1.get(x).getTeamid().toString());
+            }
+            if (testlist1.get(x).getTeamName().equals(team2name))
+            {
+                team2id =  testlist.get(x).getTeamid();
+                System.out.println("Found team 2 id : " + team2id);
+                //Log.i("Id of team",testlist1.get(x).getTeamid().toString());
+            }
+
+        }
 
     }
-
 
 }
 
